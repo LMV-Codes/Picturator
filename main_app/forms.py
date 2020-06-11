14114b0ca190a82de3599c27e53ae2b6d457ccfe
LMV-Codes <vazquez.leo.m@gmail.com>
2020-06-11 18:56:48 -0300

    Picturator 1.0

--- main_app/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-
-class ImageFieldForm(forms.Form):
-    image_field = forms.ImageField(
-        widget=forms.ClearableFileInput(attrs={'multiple': True}))
--- main_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import HomePageView, ImageListView, ImageDetailView
-
-
-urlpatterns = [
-    path('', HomePageView.as_view(), name="home"),
-    path('images/', ImageListView.as_view(), name="list_view"),
-    path('<uuid:pk>', ImageDetailView.as_view(), name="detail_view"),
-]
